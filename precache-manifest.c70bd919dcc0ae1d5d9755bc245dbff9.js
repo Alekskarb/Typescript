2f294f9136686bf87338854b683c2e5872c2f4bd
Alekskarb <korbutowski@gmail.com>
2020-04-30 12:37:35 +0300

    Updates

---
+++ precache-manifest.c70bd919dcc0ae1d5d9755bc245dbff9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4d3f4ac5d28aa32a8402f610569cf16",
+    "url": "/Typescript/index.html"
+  },
+  {
+    "revision": "ad7cf0634cb24b3935bb",
+    "url": "/Typescript/static/css/main.2d7c6b4b.chunk.css"
+  },
+  {
+    "revision": "36c5ce625a20660545fc",
+    "url": "/Typescript/static/js/2.ad871b77.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/Typescript/static/js/2.ad871b77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad7cf0634cb24b3935bb",
+    "url": "/Typescript/static/js/main.2fb31014.chunk.js"
+  },
+  {
+    "revision": "a212456ec53f0872eb07",
+    "url": "/Typescript/static/js/runtime-main.d5e61dd9.js"
+  }
+]);
